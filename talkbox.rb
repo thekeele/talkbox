8430ba448f20fc4a7ddea8fd68422ff56aea9b8a
Captain Underpants <mkeele@indiana.edu>
2014-01-15 00:31:55 -0500

    original talkbox

--- talkbox.rb
+++ talkbox.rb
@@ -1,16 +1,25 @@
+#!/usr/bin/env ruby
+require 'optparse'
+
 #ruby talkbox for mac os x
 #mRAK
 
 #read volume amt from args if present
-volume_amt = 5
+volume_amt = 10
 set_volume = `osascript -e 'set volume #{volume_amt}'`
 
 voices = ["Albert", "Zarvox", "Alex", "Bruce", "Princess", "Ralph", "Vicki", "Victoria", "Whisper"]
 #random_voice = ["Albert", "Zarvox", "Alex", "Bruce", "Princess", "Ralph", "Vicki", "Victoria", "Whisper"].sample
 
 #read talk from args if present
-talk = "Nathan"
+talk = "Welcome to Machine Learning"
 
 #add flag for random
 talkbox = `say "#{talk}" -v "#{voices[1]}"`
 #random_talkbox = `say "#{talk}" -v "#{random_voice}"`
+
+
+#drink some rum you sailor
+
+#talk like a [input]
+
